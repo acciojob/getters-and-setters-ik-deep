db854be0da246bc5861147affe089ca0d6b9569e
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-17 16:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 class Person {
-  Person(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  set age(age) {
-    this._age = age;
-  }
+    set age(age) {
+        this._age = age;
+    }
 }
 
 class Student extends Person {
-	const person = new Person("John",25);
-  study() {
-    console.log(`${person._name} is studying.`);
-  }
+    study() {
+        console.log(`${this._name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	const student = new Student("John",30)
-  teach() {
-    console.log(`${student._name} is teaching.`);
-  }
+    teach() {
+        console.log(`${this._name} is teaching`);
+    }
 }
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
